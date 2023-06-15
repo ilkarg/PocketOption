2e0905b5d8086c7d2b9bbd8bf60e2f9756567f94
Ilya_Kargapolov <ilya.kargapolov02@mail.ru>
2023-06-15 23:55:23 +0600

    Начата верстка главной страницы

---
+++ android/app/src/main/kotlin/com/icebergweb/pocket_option/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.icebergweb.pocket_option
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pocket Option',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color(0xFF1F2023),
+        fontFamily: 'SourceCodePro'
+      ),
+      home: MyHomePage(title: 'Pocket Option'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String value = "EUR/USD";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Center(
+              child: Text('Trade', style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w700,
+                  color: Color(0xFFffffff)
+              )),
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 20, left: 20),
+                  child: Text(r'Balance: $ 56 799.24', style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xFFffffff)
+                  )),
+                )
+              ],
+            ),
+            Center(
+              child: Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(top: 12, left: 20, right: 20),
+                    child: TextButton(
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            value,
+                            style: TextStyle(
+                              color: Color(0xFFffffff),
+                              fontSize: 14,
+                              fontWeight: FontWeight.w700
+                            )
+                          )
+                        ),
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(Color(0xFF121212)),
+                          fixedSize: MaterialStateProperty.all<Size>(Size(MediaQuery.of(context).size.width - 40, 32)),
+                          shape: MaterialStateProperty.resolveWith(
+                                  (states) => RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(24),
+                                  )
+                          )
+                        ),
+                        onPressed: () => debugPrint("Clicked!")
+                    )
+                  )
+                ],
+              )
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 12, left: 20),
+                  child: Text(
+                    "1.34899",
+                    style: TextStyle(
+                      color: Color(0xFFffffff),
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600
+                    )
+                  )
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 8, left: 20),
+                  child: Text(
+                    "+3.54% (+0,00034)",
+                    style: TextStyle(
+                      color: Color(0xFF58CF56),
+                      fontSize: 13,
+                      fontWeight: FontWeight.w600
+                    )
+                  )
+                )
+              ]
+            )
+          ],
+        )
+      )
+    );
+  }
+}
